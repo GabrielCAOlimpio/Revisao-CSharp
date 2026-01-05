7cb860751e35ba3a6672bab764a8a908d323c883
Gabriel Cassio <gabrielolimpio0811@gmail.com>
2026-01-05 12:31:28 -0300

    Entrada e Saida C#

---
+++ Ex4/console1/Program.cs
@@ -0,0 +1,25 @@
+﻿//ENTRADA E SAIDA
+//Input and Output
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Output
+        Console.WriteLine($"Hello, Nice to meet you"); //There is a line break
+        Console.Write($"What's your name: "); //There is no a line break
+
+        //Input
+        string? nome = Console.ReadLine();
+
+        //Output
+        if (nome != null && !nome.IsWhiteSpace())
+        {
+            Console.WriteLine($"Good Morning, {nome}!");
+        }
+        else
+        {
+            Console.WriteLine($"Sorry, I didn't understand your name.");
+        }
+    }
+}
---
+++ Ex4/console2/Program.cs
@@ -0,0 +1,35 @@
+﻿//CONVERSÃO DE DADOS
+//DATA CONVERSION
+
+class Program
+{
+    static void Main ( string[] args)
+    {
+        Console.WriteLine($"Hello! Nice to meet you.");
+        Console.Write($"What's your name: ");
+
+        string? nome = Console.ReadLine();
+
+        if (nome != null && !nome.IsWhiteSpace())
+        {
+            Console.WriteLine($"Nice to meet you {nome}");
+            Console.Write($"How old are you: ");
+            string? entrada = Console.ReadLine();
+            bool resultado = int.TryParse(entrada, out int idade);
+
+            if (resultado)
+            {
+                Console.WriteLine(idade >= 18 ? "You are an adult" : "You are a kid");
+            }
+            else
+            {
+                Console.WriteLine("Sorry, I didn't understand your age.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Sorry, I didn't understand your name.");
+        }
+        
+    }
+}
