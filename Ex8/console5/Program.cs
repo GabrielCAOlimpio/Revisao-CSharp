16ba25f0a30633f8f1143800ec40c1658150fdf8
Gabriel Olimpio <gabrielolimpio0811@gmail.com>
2026-01-08 16:20:30 -0300

    Polimorfismo

---
+++ Ex8/console5/Program.cs
@@ -0,0 +1,28 @@
+﻿//Herança e Polimorfismo em C#
+//Poliformism and Inheritance in C#
+
+using System;
+
+namespace Program;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<Pessoa> pessoas = new List<Pessoa>()
+        {
+            new Aluno("Ana", 20, "Engenharia"),
+            new Aluno("Bruno", 22, "Medicina"),
+            new Aluno("Carlos", 21, "Direito"),
+            new Pessoa("Lucas", 30),
+            new Pessoa("Mariana", 28),
+            new Pessoa("Pedro", 35)
+        };
+        foreach (var pessoa in pessoas)
+        {
+            pessoa.Apresentar(); // Same method, different behavior
+            Console.WriteLine();
+        }
+    }
+}
+
