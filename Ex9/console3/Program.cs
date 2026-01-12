411c0ac808f3d50eba253d92ce499b28ffe83e5f
Gabriel Olimpio <gabrielolimpio0811@gmail.com>
2026-01-12 13:31:20 -0300

    HashSets em C#

--- Ex9/console3/Program.cs
+++ Ex9/console3/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//HASHSET IN C#;
+//HASHSET EM C#;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        HashSet<int> numbers = [1,2,3,4,5]; //A moderna sintaxe de inicialização de HashSet em C# usa colchetes.
+        
+        //Adicionando elementos
+        numbers.Add(6);
+        numbers.Add(7);
+        Console.WriteLine($"Numeros no Hashset : {string.Join(",",numbers)}");
+
+        bool tryToAdd = numbers.Add(3); //Tentar adicionar um elemento duplicado
+        //No hashset, elementos duplicados não são permitidos.
+        Console.WriteLine($"Tentativa de adicionar 3 novamente: {(tryToAdd ? "Sucesso" : "Falha - Duplicado")}");
+
+        //Removendo elementos
+        numbers.Remove(4);
+        Console.WriteLine($"Numeros após remover 4: {string.Join(",",numbers)}");
+
+        //Verificando a existência de um elemento
+        bool containsFive = numbers.Contains(5); //É muito mais rapido do que em uma lista.
+        Console.WriteLine($"Hashset contém 5: {(containsFive ? "Sim" : "Não")}");
+    }
+}
