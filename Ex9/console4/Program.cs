c27e55681961e1e1d7facc4763285f8712e4c2e5
Gabriel Olimpio <gabrielolimpio0811@gmail.com>
2026-01-12 13:51:07 -0300

    Queue em C#

---
+++ Ex9/console4/Program.cs
@@ -0,0 +1,26 @@
+﻿//QUEUE EM C# 
+
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Queue<string> fila = new(); // Cria uma fila de strings
+        fila.Enqueue("Elemento 1"); // Adiciona elementos à fila (sempre no final)
+        fila.Enqueue("Elemento 2");
+        fila.Enqueue("Elemento 3");
+        Console.WriteLine($"Elementos na fila: {string.Join(", ", fila)}");
+        Console.WriteLine();
+
+        //Em uma queue o primeiro elemento a entrar é o primeiro a sair (FIFO - First In, First Out)
+        string proximoElemento = fila.Peek(); //Retorna o proximo elemento sem removê-lo
+        Console.WriteLine($"Próximo elemento a ser removido: {proximoElemento}");
+        Console.WriteLine();
+
+        //Remove o primeiro elemento da fila (FIFO)
+        string atendido = fila.Dequeue();
+        Console.WriteLine($"Elemento removido: {atendido}");
+        Console.WriteLine($"Elementos restantes na fila: {string.Join(", ", fila)}");
+    }
+}
